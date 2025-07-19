501bacc4db6314934d96d712049e82018deb0532
israhjf <hjfisrael@gmail.com>
2025-07-19 10:35:55 -0600

    Initial Commit

---
+++ DemoProductReview/DemoProductReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProductReviewApp.swift
+//  DemoProductReview
+//
+//  Created by Israel on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoProductReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
